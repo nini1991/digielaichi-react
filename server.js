692416b1c4bc6422e23dc84b6ae94a8068afb788
NIthin Antony <nithinparathara007@gmail.com>
2019-03-14 20:46:13 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
 //server.js
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-   
 //Install express server
 const express = require('express');
 const path = require('path');
