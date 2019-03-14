a190edd6137c90b3cd942d9eea8014423f2c330d
NIthin Antony <nithinparathara007@gmail.com>
2019-03-14 20:45:04 +0530

    server.js updated

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+   
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/build'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/build/index.html'));
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
